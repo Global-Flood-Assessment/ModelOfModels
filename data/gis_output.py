8968b5f2eccf372205de959202a6e71558ad736f
Jun Wang <wang208@indiana.edu>
2020-06-27 15:17:24 -0400

    #3 add gis output as zip file

--- data/gis_output.py
+++ data/gis_output.py
@@ -4,6 +4,7 @@
 """
 
 import os,sys,json,csv
+import zipfile
 
 import pandas as pd
 import geopandas
@@ -23,7 +24,24 @@ def generate_gisfile(flood_csv,real_date,out_folder):
     out_df = out_df.merge(warning_df, left_on='pfaf_id', right_on='pfaf_id')
     # write warning result to geojson
     result_geojson = out_folder + "flood_warning_" + real_date + ".geojson"
-    out_df.to_file(result_geojson, index=True, driver='GeoJSON')
+    out_df.to_file(result_geojson, index=True, driver='GeoJSON')    # generate a zip file
+    
+    # flood_warning_20200617.cpg
+    # flood_warning_20200617.prj
+    # flood_warning_20200617.dbf
+    # flood_warning_20200617.shp
+    # flood_warning_20200617.geojson 
+    # flood_warning_20200617.shx
+    # flood_warning_20200617.kml
+    file_prefix = out_folder + "flood_warning_" + real_date
+    file_types = ['cpg','prj','dbf','shp','shx','geojson','kml']
+    zip_file = file_prefix + ".zip"
+    with zipfile.ZipFile(zip_file, 'w',zipfile.ZIP_DEFLATED) as zipObj:
+        for ftype in file_types:
+            filename = file_prefix + "." + ftype 
+            # Add file to zip
+            zipObj.write(filename, os.path.basename(filename))
+
     # write warning result to shapefile
     result_shp = out_folder + "flood_warning_" + real_date + ".shp"
     out_df.to_file(result_shp, index=True)
@@ -31,8 +49,10 @@ def generate_gisfile(flood_csv,real_date,out_folder):
     result_kml = out_folder + "flood_warning_" + real_date + ".kml"
     # !ogr2ogr -f 'KML' -a_srs EPSG:4326 $result_kml $result_geojson
     geopandas.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+    geopandas.io.file.fiona.drvsupport.supported_drivers['LIBKML'] = 'rw'
     out_df.to_file(result_kml, driver='KML')
-    
+
+
     return 
 
 def main():
