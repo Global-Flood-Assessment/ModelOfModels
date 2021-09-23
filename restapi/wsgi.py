6c0dcf01445311b80f72cc6402c53f71b1d86f86
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2021-09-23 11:15:37 -0400

    rewrite restapi

--- restapi/wsgi.py
+++ restapi/wsgi.py
@@ -4,5 +4,14 @@
 		-- run as flask wrapper as wsgi application
 		-- run as non-root user: ~/miniconda3/bin/mod_wsgi-express start-server wsgi.py &
 """
+##################
+# FOR PRODUCTION
+##################
 
 from MoM_service_API import app as application
+
+if __name__ == "__main__":
+    ####################
+    # FOR DEVELOPMENT
+    ####################
+    application.run(host='0.0.0.0', debug=True)
