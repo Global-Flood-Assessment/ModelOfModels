8cfc65318a431db8cce77612ea9bf09da2e512ca
webgisdeveloper <wang208@indiana.edu>
2021-10-17 18:04:39 -0400

    restapi: DFO ...

--- restapi/src/endpoints/dataConfig.py
+++ restapi/src/endpoints/dataConfig.py
@@ -15,6 +15,9 @@ conf_dict['HWRF'] = home + "/ModelofModels/data/cron_data/HWRF/HWRF_MoM/"
 # HH can be 00,06,12,18
 conf_dict['HWRF_MoM'] = "Attributes_Clean_{date}HWRFUpdated.csv"
 
+conf_dict['DFO'] = home + "/ModelofModels/data/cron_data/DFO/DFO_MoM/"
+conf_dict['DFO_MoM'] = "Attributes_Clean_{date}MOM+DFOUpdated.csv" 
+
 conf_dict['VIIRS'] = home + "/ModelofModels/data/cron_data/VIIRS/VIIRS_MoM/"
 # HH can be 18
 conf_dict['VIIRS_MoM'] = "Attributes_clean_{date}MOM+DFO+VIIRSUpdated.csv"
