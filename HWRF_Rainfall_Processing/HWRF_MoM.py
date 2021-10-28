1680dc2ed0374783d8fab6a7054e006eb847cfc7
webgisdeveloper <wang208@indiana.edu>
2021-10-28 19:42:49 -0400

    #19 everything in one script

--- HWRF_Rainfall_Processing/HWRF_MoM.py
+++ HWRF_Rainfall_Processing/HWRF_MoM.py
@@ -319,6 +319,243 @@ def update_HWRF_MoM(adate,gfmsfolder,glofasfolder,hwrffolder,outputfolder):
     except:
         pass
 
+def find_latest_summary(datestr, scanfolder,namepattern):
+    """ find the latest summary data"""
+
+    # turn the datestr into a real date
+    da = datetime.strptime(datestr,"%Y%m%d")
+
+    # check the data
+    for i in range(10):
+        cdate = da - timedelta(days=i)
+        cdatestr = cdate.strftime("%Y%m%d")
+        cfile = scanfolder + namepattern.format(cdatestr)
+        if os.path.exists(cfile):
+            return [cdatestr, cfile]
+    
+    return
+
+def mofunc_dfo(row):
+    if row['Severity'] > 0.8 or row['Hazard_Score'] > 80:
+        return 'Warning'
+    elif 0.6 < row['Severity'] < 0.80 or 60 < row['Hazard_Score'] < 80:
+        return 'Watch'
+    elif 0.35 < row['Severity'] < 0.6 or 35 < row['Hazard_Score'] < 60:
+        return 'Advisory'
+    elif 0 < row['Severity'] < 0.35 or 0 < row['Hazard_Score'] < 35:
+        return 'Information'
+
+def mofunc_viirs(row):
+    if row['Severity'] > 0.8 or row['Hazard_Score'] > 80:
+        return 'Warning'
+    elif 0.6 < row['Severity'] < 0.80 or 60 < row['Hazard_Score'] < 80:
+        return 'Watch'
+    elif 0.35 < row['Severity'] < 0.6 or 35 < row['Hazard_Score'] < 60:
+        return 'Advisory'
+    elif 0 < row['Severity'] < 0.35 or 0 < row['Hazard_Score'] < 35:
+        return 'Information'
+
+def update_HWRFMoM_DFO_VIIRS(adate,HWRF_f,DFO_f,VIIRS_f,outputdir):
+    """
+        update HWRFMoM with the latest available DFO and VIIRS 
+    """
+
+    # first check if it is produced
+    hwrf_pattern = adate+"HWRF"
+    file_list = os.listdir(outputdir)
+    file_found = [1 for x in file_list if hwrf_pattern in x]
+    if len(file_found) > 0:
+        # this date is already processed
+        return
+
+    # adate 
+    datestr = adate[:-2]
+
+    [dfo_date,dfo_summary] = find_latest_summary(datestr,DFO_f,"DFO_{}.csv")
+    [viirs_date,viirs_summary] = find_latest_summary(datestr,VIIRS_f,"VIIRS_Flood_{}.csv")
+
+    # geneate HWRF_DFO_MoM
+    MOMOutput= HWRF_f + 'Final_Attributes_{}HWRFUpdated.csv'.format(adate)
+    if not os.path.exists(MOMOutput):
+        print('can not find ' + MOMOutput)
+        return
+    
+    DFO = dfo_summary
+    #output
+    #Final_Attributes_yyyymmddhhMOM+yyyymmddDFOUpdated.csv
+    #Attributes_clean_yyyymmddhhMOM+yyyymmddDFOUpdated.csv 
+    DFO_Final_Attributes_csv = 'Final_Attributes_{}HWRF+{}DFOUpdated.csv'.format(adate,dfo_date)
+    DFO_Attributes_Clean_csv = 'Attributes_Clean_{}HWRF+{}DFOUpdated.csv'.format(adate,dfo_date)
+    
+    weightage = read_data('Weightage_DFO.csv')
+    Attributes=read_data('Attributes.csv')
+    PDC_resilience = read_data('Copy of Resilience_Index.csv')
+    add_field_DFO=['DFO_area_1day_score', 'DFO_percarea_1day_score', 'DFO_area_2day_score', 'DFO_percarea_2day_score','DFO_area_3day_score', 'DFO_percarea_3day_score','DFOTotal_Score']
+
+    #Read DFO Processing data and calculate score
+    with open(DFO, 'r', encoding='UTF-8') as DFO_file:
+        DFO_reader = csv.reader(DFO_file)
+        DFO_w_score_csv = "DFO_w_score_{}.csv".format(dfo_date)
+        csvfile = open(DFO_w_score_csv, 'w', newline='\n', encoding='utf-8')
+        DFO_w_score = csv.writer(csvfile)
+        row_count = 1
+        # csv_writer = csv.writer(write_obj)
+        for row in DFO_reader:
+            if row_count == 1:
+                for x in add_field_DFO:
+                    row.append(x)
+                row_count = row_count + 1
+            else:
+                if float(row[4]) / float(weightage.DFO_Area_wt) > float(weightage.DFO_Area_max_pt):
+                    DFO_area_1day_score = str(float(weightage.DFO_Area_max_pt)*float(weightage.one_Day_Multiplier))
+                else:
+                    DFO_area_1day_score = str(float(weightage.DFO_Area_Min_pt) * float(weightage.one_Day_Multiplier)* float(row[4]) / float(weightage.DFO_Area_wt))
+                if float(row[5]) / float(weightage.DFO_percArea_wt) > float(weightage.DFO_percArea_Maxpt):
+                    DFO_perc_area_1day_score = str(float(weightage.DFO_percArea_Maxpt)*float(weightage.one_Day_Multiplier))
+                else:
+                    DFO_perc_area_1day_score = str(float(weightage.DFO_percArea_Minpt)*float(weightage.one_Day_Multiplier)* float(row[5]) / float(weightage.DFO_percArea_wt))
+                if float(row[6]) / float(weightage.DFO_Area_wt) > float(weightage.DFO_Area_max_pt):
+                    DFO_area_2day_score = str(float(weightage.DFO_Area_max_pt)*float(weightage.two_Day_Multiplier))
+                else:
+                    DFO_area_2day_score = str(float(weightage.DFO_Area_Min_pt) * float(weightage.two_Day_Multiplier)* float(row[6]) / float(weightage.DFO_Area_wt))
+                if float(row[7]) / float(weightage.DFO_percArea_wt) > float(weightage.DFO_percArea_Maxpt):
+                    DFO_perc_area_2day_score = str(float(weightage.DFO_percArea_Maxpt)*float(weightage.two_Day_Multiplier))
+                else:
+                    DFO_perc_area_2day_score = str(float(weightage.DFO_percArea_Minpt)*float(weightage.two_Day_Multiplier)* float(row[7]) / float(weightage.DFO_percArea_wt))
+                if float(row[8]) / float(weightage.DFO_Area_wt) > float(weightage.DFO_Area_max_pt):
+                    DFO_area_3day_score = str(float(weightage.DFO_Area_max_pt)*float(weightage.three_Day_Multiplier))
+                else:
+                    DFO_area_3day_score = str(float(weightage.DFO_Area_Min_pt) * float(weightage.three_Day_Multiplier)* float(row[8]) / float(weightage.DFO_Area_wt))
+                if float(row[9]) / float(weightage.DFO_percArea_wt) > float(weightage.DFO_percArea_Maxpt):
+                    DFO_perc_area_3day_score = str(float(weightage.DFO_percArea_Maxpt)*float(weightage.three_Day_Multiplier))
+                else:
+                    DFO_perc_area_3day_score = str(float(weightage.DFO_percArea_Minpt)*float(weightage.three_Day_Multiplier)* float(row[9]) / float(weightage.DFO_percArea_wt))
+                                            
+                Sum_Score = str(
+                    (float(DFO_area_1day_score) + float(DFO_perc_area_1day_score) + float(DFO_area_2day_score) + float(DFO_perc_area_2day_score)+float(DFO_area_3day_score) + float(DFO_perc_area_3day_score)))
+                score_field = [DFO_area_1day_score, DFO_perc_area_1day_score, DFO_area_2day_score, DFO_perc_area_2day_score, DFO_area_3day_score, DFO_perc_area_3day_score,Sum_Score]
+                for x in score_field:
+                    row.append(x)
+            DFO_w_score.writerow(row)
+    csvfile.close()
+
+    DFO = read_data(DFO_w_score_csv)
+    DFO = DFO[DFO.DFOTotal_Score > 0.1]
+    DFO = DFO.iloc[:,1:]
+    MOM = read_data(MOMOutput)
+    MOM.drop(columns=['area_km2','ISO','Admin0','Admin1','rfr_score','cfr_score','Resilience_Index',' NormalizedLackofResilience ','Severity','Alert'], inplace=True)
+    Final_Output_0= pd.merge(MOM.set_index('pfaf_id'), DFO.set_index('pfaf_id'), on='pfaf_id', how='outer')
+    join1 = pd.merge(Attributes, PDC_resilience[['ISO', 'Resilience_Index', ' NormalizedLackofResilience ']], on='ISO', how='inner')
+    Final_Output=pd.merge(join1.set_index('pfaf_id'), Final_Output_0, on='pfaf_id', how='outer')
+    Final_Output[['Hazard_Score']] = Final_Output[['Hazard_Score']].fillna(value=0)
+    Final_Output.loc[(Final_Output['Hazard_Score']<Final_Output['DFOTotal_Score']),'Flag']=2
+    Final_Output['Hazard_Score'] =Final_Output[['Hazard_Score', 'DFOTotal_Score']].max(axis=1)
+    Final_Output = Final_Output[Final_Output.Hazard_Score != 0]
+    Final_Output.drop(Final_Output.index[(Final_Output['rfr_score']==0) & (Final_Output['cfr_score']==0)], inplace=True)
+    Final_Output = Final_Output.assign(
+        Scaled_Riverine_Risk=lambda x: Final_Output['rfr_score'] * 20)
+    Final_Output = Final_Output.assign(
+        Scaled_Coastal_Risk=lambda x: Final_Output['cfr_score'] * 20)
+    Final_Output = Final_Output.assign(
+        Severity=lambda x: scipy.stats.norm(np.log(100 - Final_Output[['Scaled_Riverine_Risk', 'Scaled_Coastal_Risk']].max(axis=1)), 1).cdf(
+            np.log(Final_Output['Hazard_Score'])))
+    Final_Output['Alert'] = Final_Output.apply(mofunc_dfo, axis=1)
+    Final_Output.loc[Final_Output['Alert']=="Information",'Flag']=''
+    Final_Output.loc[Final_Output['Alert']=="Advisory",'Flag']=''
+    Final_Output.to_csv(DFO_Final_Attributes_csv, encoding='utf-8-sig')
+    #Final_Output.to_csv('Final_Attributes_20210701_DFOUpdated.csv', encoding='utf-8-sig')
+    join1 = pd.merge(Attributes, PDC_resilience[['ISO', 'Resilience_Index', ' NormalizedLackofResilience ']], on='ISO', how='inner')
+    Attributes_Clean_DFO_Updated = pd.merge(join1.set_index('pfaf_id'), Final_Output[['Alert','Flag']], on='pfaf_id', how='right')
+    Attributes_Clean_DFO_Updated.to_csv(DFO_Attributes_Clean_csv, encoding='utf-8-sig')
+    os.remove(DFO_w_score_csv)
+
+    # generate HWRF_DFO_VIIRS_MoM
+    DFO_MOMOutput = DFO_Final_Attributes_csv
+    VIIRS_summary_csv = viirs_summary
+
+    #output files
+    Final_Attributes_csv = "Final_Attributes_{}HWRF+{}DFO+{}VIIRSUpdated.csv".format(adate,dfo_date,viirs_date)
+    Final_Attributes_csv = outputdir + Final_Attributes_csv
+    Attributes_Clean_csv = "Attributes_clean_{}HWRF+{}DFO+{}VIIRSUpdated.csv".format(adate,dfo_date,viirs_date)
+    Attributes_Clean_csv = outputdir + Attributes_Clean_csv
+    
+    weightage = read_data('VIIRS_Weightages.csv')
+    Attributes=read_data('Attributes.csv')
+    PDC_resilience = read_data('Copy of Resilience_Index.csv')
+    add_field_VIIRS=['VIIRS_area_1day_score', 'VIIRS_percarea_1day_score', 'VIIRS_area_5day_score', 'VIIRS_percarea_5day_score','VIIRSTotal_Score']
+
+    #Read VIIRS Processing data and calculate score
+    with open(VIIRS_summary_csv, 'r', encoding='UTF-8') as VIIRS_file:
+        VIIRS_reader = csv.reader(VIIRS_file)
+        VIIRS_w_score_csv ="VIIRS_w_score_{}.csv".format(viirs_date)
+        csvfile = open(VIIRS_w_score_csv, 'w', newline='\n', encoding='utf-8')
+        VIIRS_w_score = csv.writer(csvfile)
+        row_count = 1
+        # csv_writer = csv.writer(write_obj)
+        for row in VIIRS_reader:
+            if row_count == 1:
+                for x in add_field_VIIRS:
+                    row.append(x)
+                row_count = row_count + 1
+            else:
+                if float(row[1]) / float(weightage.VIIRS_Area_wt) > float(weightage.VIIRS_Area_max_pt):
+                    VIIRS_area_1day_score = str(float(weightage.VIIRS_Area_max_pt)*float(weightage.one_Day_Multiplier))
+                else:
+                    VIIRS_area_1day_score = str(float(weightage.VIIRS_Area_Min_pt) * float(weightage.one_Day_Multiplier)* float(row[1]) / float(weightage.VIIRS_Area_wt))
+                if float(row[2]) / float(weightage.VIIRS_percArea_wt) > float(weightage.VIIRS_percArea_Maxpt):
+                    VIIRS_perc_area_1day_score = str(float(weightage.VIIRS_percArea_Maxpt)*float(weightage.one_Day_Multiplier))
+                else:
+                    VIIRS_perc_area_1day_score = str(float(weightage.VIIRS_percArea_Minpt)*float(weightage.one_Day_Multiplier)* float(row[2]) / float(weightage.VIIRS_percArea_wt))
+                if float(row[3]) / float(weightage.VIIRS_Area_wt) > float(weightage.VIIRS_Area_max_pt):
+                    VIIRS_area_5day_score = str(float(weightage.VIIRS_Area_max_pt)*float(weightage.five_Day_Multiplier))
+                else:
+                    VIIRS_area_5day_score = str(float(weightage.VIIRS_Area_Min_pt) * float(weightage.five_Day_Multiplier)* float(row[3]) / float(weightage.VIIRS_Area_wt))
+                if float(row[4]) / float(weightage.VIIRS_percArea_wt) > float(weightage.VIIRS_percArea_Maxpt):
+                    VIIRS_perc_area_5day_score = str(float(weightage.VIIRS_percArea_Maxpt)*float(weightage.five_Day_Multiplier))
+                else:
+                    VIIRS_perc_area_5day_score = str(float(weightage.VIIRS_percArea_Minpt)*float(weightage.five_Day_Multiplier)* float(row[4]) / float(weightage.VIIRS_percArea_wt))          
+                Sum_Score = str(
+                    (float(VIIRS_area_1day_score) + float(VIIRS_perc_area_1day_score) + float(VIIRS_area_5day_score) + float(VIIRS_perc_area_5day_score)))
+                score_field = [VIIRS_area_1day_score, VIIRS_perc_area_1day_score, VIIRS_area_5day_score, VIIRS_perc_area_5day_score, Sum_Score]
+                for x in score_field:
+                    row.append(x)
+            VIIRS_w_score.writerow(row)
+    csvfile.close()
+
+    VIIRS = read_data(VIIRS_w_score_csv)
+    VIIRS = VIIRS[VIIRS.VIIRSTotal_Score > 0.1]
+    MOM = read_data(DFO_MOMOutput)
+    MOM.drop(columns=['area_km2','ISO','Admin0','Admin1','rfr_score','cfr_score','Resilience_Index',' NormalizedLackofResilience ','Severity','Alert'], inplace=True)
+    Final_Output_0= pd.merge(MOM.set_index('pfaf_id'), VIIRS.set_index('pfaf_id'), on='pfaf_id', how='outer')
+    join1 = pd.merge(Attributes, PDC_resilience[['ISO', 'Resilience_Index', ' NormalizedLackofResilience ']], on='ISO', how='inner')
+    Final_Output=pd.merge(join1.set_index('pfaf_id'), Final_Output_0, on='pfaf_id', how='right')
+    Final_Output[['Hazard_Score']] = Final_Output[['Hazard_Score']].fillna(value=0)
+    Final_Output.loc[(Final_Output['Hazard_Score']<Final_Output['VIIRSTotal_Score']),'Flag']=3
+    Final_Output['Hazard_Score'] =Final_Output[['Hazard_Score', 'VIIRSTotal_Score']].max(axis=1)
+    Final_Output = Final_Output[Final_Output.Hazard_Score != 0]
+    Final_Output.drop(Final_Output.index[(Final_Output['rfr_score']==0) & (Final_Output['cfr_score']==0)], inplace=True)
+    Final_Output = Final_Output.assign(
+        Scaled_Riverine_Risk=lambda x: Final_Output['rfr_score'] * 20)
+    Final_Output = Final_Output.assign(
+        Scaled_Coastal_Risk=lambda x: Final_Output['cfr_score'] * 20)
+    Final_Output = Final_Output.assign(
+        Severity=lambda x: scipy.stats.norm(np.log(100 - Final_Output[['Scaled_Riverine_Risk', 'Scaled_Coastal_Risk']].max(axis=1)), 1).cdf(
+            np.log(Final_Output['Hazard_Score'])))
+    Final_Output['Alert'] = Final_Output.apply(mofunc_viirs, axis=1)
+    Final_Output.loc[Final_Output['Alert']=="Information",'Flag']=''
+    Final_Output.loc[Final_Output['Alert']=="Advisory",'Flag']=''
+    Final_Output.to_csv(Final_Attributes_csv, encoding='utf-8-sig')
+    join1 = pd.merge(Attributes, PDC_resilience[['ISO', 'Resilience_Index', ' NormalizedLackofResilience ']], on='ISO', how='inner')
+    Attributes_Clean_VIIRS_Updated = pd.merge(join1.set_index('pfaf_id'), Final_Output[['Alert','Flag']], on='pfaf_id', how='right')
+    Attributes_Clean_VIIRS_Updated.to_csv(Attributes_Clean_csv, encoding='utf-8-sig')
+    os.remove(VIIRS_w_score_csv)
+
+    # remove DFO output
+    os.remove(DFO_Final_Attributes_csv)
+    os.remove(DFO_Attributes_Clean_csv)
+
+    return
+
 def main():
     # run batch mode
 
@@ -330,6 +567,12 @@ def main():
     glofasf = home + "/ModelofModels/data/cron_data/glofas/"
     hwrff = home + "/ModelofModels/data/cron_data/HWRF/HWRF_summary/"
     outputf = home + "/ModelofModels/data/cron_data/HWRF/HWRF_MoM/"
+    dfof= home + "/ModelofModels/data/cron_data/DFO/DFO_summary/"
+    viirsf = home + "/ModelofModels/data/cron_data/VIIRS/VIIRS_summary/"
+    hwrf_hourf = home + "/ModelofModels/data/cron_data/HWRF/HWRF_DFO_VIIRS_MoM/"
+    # debug
+    update_HWRFMoM_DFO_VIIRS(testdate,hwrff,dfof,viirsf,hwrf_hourf)
+    return    
     
     for entry in sorted(os.listdir(hwrff))[-6:]:
         # extract adate
