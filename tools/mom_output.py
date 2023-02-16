08c060c162fe6a7c6afa5ac43e21ba144b208a4c
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2023-02-16 13:16:06 -0500

    merge alert value

--- tools/mom_output.py
+++ tools/mom_output.py
@@ -27,6 +27,8 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+MOM_DOWNLOAD_FOLDER = "pdc_final"
+
 
 def download_mom(starttime, endtime):
     """download mom output in a time period"""
@@ -44,18 +46,48 @@ def download_mom(starttime, endtime):
             continue
         if fstr >= start_mom and fstr <= end_mom:
             mom_list.append(fstr)
-            dataurl = os.path.join(baseurl, fstr)
-            wgetcmd = "wget -nc " + dataurl + " -P pdc_final"
-            os.system(wgetcmd)
+            if not os.path.exists(os.path.join(MOM_DOWNLOAD_FOLDER, fstr)):
+                dataurl = os.path.join(baseurl, fstr)
+                wgetcmd = "wget -nc " + dataurl + " -P " + MOM_DOWNLOAD_FOLDER
+                os.system(wgetcmd)
     mom_list.sort()
     return mom_list
 
 
-def extract_mom(csvlist, timeperiod, outputfolder):
+def merge_mom(df_ids, momfiles):
+    """merge mom output to one csv"""
+
+    joined_df = df_ids
+    joined_df.set_index("pfaf_id", inplace=True)
+
+    total_file = len(momfiles)
+    for count, mom in enumerate(momfiles):
+        print("processing {} / {}".format(count, total_file))
+        mom_df = pd.read_csv(
+            os.path.join(MOM_DOWNLOAD_FOLDER, mom), encoding="ISO-8859-1"
+        )
+        mom_df = mom_df[["pfaf_id", "Severity"]]
+        mom_df = mom_df.drop_duplicates(subset=["pfaf_id"])
+        # Final_Attributes_2022081606HWRF+MOM+DFO+VIIRSUpdated_PDC.csv
+        datestr = mom[17 : 17 + 10]
+        # rename alert
+        mom_df.rename(columns={"Severity": datestr}, inplace=True)
+        mom_df.set_index("pfaf_id", inplace=True)
+        # merge stuff
+        joined_df = joined_df.join(mom_df, how="left")
+
+    joined_df.to_csv("output.csv", index=True, float_format="%.3f")
+
+
+def extract_mom(csvfile, timeperiod, outputfolder):
     """extract mom outputs"""
 
+    # load list of watersheds
+    df = pd.read_csv(csvfile)
+    df = df[["pfaf_id"]]
+
     # check the folder
-    mom_folder = os.path.join(outputfolder, "pdc_final")
+    mom_folder = os.path.join(outputfolder, MOM_DOWNLOAD_FOLDER)
     if not os.path.exists(mom_folder):
         os.makedirs(mom_folder)
     os.chdir(outputfolder)
@@ -64,6 +96,9 @@ def extract_mom(csvlist, timeperiod, outputfolder):
     start_t += "00"
     end_t += "18"
     mom_list = download_mom(start_t, end_t)
+
+    # extract mom output to one file
+    merge_mom(df, mom_list)
 
 
 def main():
