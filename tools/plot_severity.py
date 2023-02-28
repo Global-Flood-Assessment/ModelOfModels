971f484012898d6dcf1a85bd6134554a4e658c66
cicuser <webgisdeveloper>
2023-02-28 17:14:42 -0500

    severity plot with guide line

--- tools/plot_severity.py
+++ tools/plot_severity.py
@@ -13,9 +13,9 @@ num = len(list(df.columns))
 
 index_list = list(df.index.values)
 
-for aid in index_list[4:5]:
+for aid in index_list:
     imagename = os.path.join("pakistan2022", "severityplot", f"{aid}_severity.png")
-    fig, ax = plt.subplots(figsize=(6, 4))
+    fig, ax = plt.subplots(figsize=(8, 6))
     (df.loc[[aid]].T).plot.line(
         xticks=[1, int(num * 0.25), int(num * 0.50), int(num * 0.75), int(num * 0.95)],
         ylabel="Severity",
@@ -25,6 +25,9 @@ for aid in index_list[4:5]:
     plt.axhline(y=0.8, color="r", linestyle="-")
     plt.axhline(y=0.6, color="orange", linestyle="-")
     plt.axhline(y=0.35, color="y", linestyle="-")
-    # plt.savefig(imagename, bbox_inches="tight")
-    plt.show()
+    plt.text(int(num * 0.95), 0.81, "Warning", fontsize=10)
+    plt.text(int(num * 0.95), 0.61, "Watch", fontsize=10)
+    plt.text(int(num * 0.95), 0.36, "Advisory", fontsize=10)
+    plt.savefig(imagename, bbox_inches="tight")
+    # plt.show()
     plt.close("all")
